d16d7ea6627a25814588f47e99be589070c48ca6
mdnayab <mdnayabkarim141@gmail.com>
2024-10-09 22:59:34 +0530

    Signup completed

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -9,7 +9,8 @@ export async function POST(request: Request) {
   try {
     const { username, email, password } = await request.json();
 
-    const existingUserVerificationByUsername = await UserModel.findOne({            // Here we find the username is already exist and verified or not??
+    const existingUserVerificationByUsername = await UserModel.findOne({
+      // Here we find the username is already exist and verified or not??
       username,
       isVerified: true,
     });
@@ -23,12 +24,30 @@ export async function POST(request: Request) {
       );
     }
 
-    const existingUserByEmail = await UserModel.findOne({ email });             // Here we find the email is already exist or not??
+    const existingUserByEmail = await UserModel.findOne({ email }); // Here we find the email is already exist or not??
     const verifyCode = Math.floor(10000 + Math.random() * 9000).toString();
 
-    if (existingUserByEmail) {           // If the email is exist then we return the error response
-      true; //Back Here
-    } else {                             // If the email is not exist then we store the info in User model
+    if (existingUserByEmail) {
+      // If the email is exist then we return the error response
+      if (existingUserByEmail.isVerified) {
+        return Response.json(
+          {
+            success: false,
+            message: "User already exist with this email",
+          },
+          { status: 500 }
+        );
+      } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        existingUserByEmail.password = hashedPassword;
+        existingUserByEmail.verifyCode = verifyCode;
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+
+        await existingUserByEmail.save();
+      }
+    } else {
+      // If the email is not exist then we store the info in User model
       const hashedPassword = await bcrypt.hash(password, 10);
       const expiryDate = new Date();
       expiryDate.setDate(expiryDate.getHours() + 1);
@@ -44,10 +63,32 @@ export async function POST(request: Request) {
         messages: [],
       });
 
-      await newUser.save()                    // Then, save the info in database
-
-      const emailResponse = await sendVerificationEmail(email, username, verifyCode)
+      await newUser.save(); // Then, save the info in database
     }
+
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "User registered successfully. Please berify your email",
+      },
+      { status: 201 }
+    );
   } catch (error) {
     console.error("Error registering user", error);
     return Response.json(
