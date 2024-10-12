d62136fd40cd97ebf2b536a2a4760bf4b8047a4a
mdnayab <mdnayabkarim141@gmail.com>
2024-10-13 00:03:01 +0530

    Message functionality completed

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+'use client'
+import { Message } from "@/model/User"
+import { useState } from "react"
+
 
 function page() {
+  const [messages, setMessages] = useState<Message[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isSwitchLoading, setIsSwitchLoading] = useState(false)
+
   return (
     <div>page</div>
   )
