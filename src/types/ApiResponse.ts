c97662298e92d81cd3e7668dc79359f5b138e418
mdnayab <mdnayabkarim141@gmail.com>
2024-10-14 02:05:59 +0530

    Dashboard page completed

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -3,6 +3,6 @@ import { Message } from "@/model/User";
 export interface ApiResponse{            // Here we declare a type/template of Api response (src/types/ApiResponse.ts) for ease in returning the response.
     success: boolean,
     message: string,
-    isAcceptingMessages?: boolean,
+    isAcceptingMessage?: boolean,
     messages?: Array<Message>
 }
